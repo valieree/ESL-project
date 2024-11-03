b24c094ceecdcbf1cbb24d57e4df202b3f5f2b4f
valieree <valierezzz@bk.ru>
2024-11-03 19:00:35 +0000

    Using GPIO for new logic

--- blinky/main.c
+++ blinky/main.c
@@ -1,82 +1,81 @@
-/**
- * Copyright (c) 2014 - 2021, Nordic Semiconductor ASA
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form, except as embedded into a Nordic
- *    Semiconductor ASA integrated circuit in a product or a software update for
- *    such product, must reproduce the above copyright notice, this list of
- *    conditions and the following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * 4. This software, with or without modification, must only be used with a
- *    Nordic Semiconductor ASA integrated circuit.
- *
- * 5. Any software provided in binary form under this license must not be reverse
- *    engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-/** @file
- *
- * @defgroup blinky_example_main main.c
- * @{
- * @ingroup blinky_example
- * @brief Blinky Example Application main file.
- *
- * This file contains the source code for a sample application to blink LEDs.
- *
- */
-
 #include <stdbool.h>
 #include <stdint.h>
 #include "nrf_delay.h"
-#include "boards.h"
+#include "nrf_gpio.h"
 
 
-/**
- * @brief Function for application main entry.
- */
-int main(void)
+#define FIRST_LED_PIN      NRF_GPIO_PIN_MAP(0, 6)
+#define SECOND_RED_LED_PIN NRF_GPIO_PIN_MAP(0, 8)
+#define SECOND_GREEN_LED_PIN NRF_GPIO_PIN_MAP(1, 9)
+#define SECOND_BLUE_LED_PIN NRF_GPIO_PIN_MAP(0, 12)
+#define BUTTON_PIN         NRF_GPIO_PIN_MAP(1, 6)
+
+#define LED_DELAY_MS 500
+
+uint8_t led_sequence[] = {FIRST_LED_PIN, SECOND_RED_LED_PIN, SECOND_GREEN_LED_PIN, SECOND_BLUE_LED_PIN};
+uint8_t device_id[] = {6, 5, 7, 9}; 
+
+void gpio_init(void)
 {
-    /* Configure board. */
-    bsp_board_init(BSP_INIT_LEDS);
-    uint8_t device_id[] = {6, 5, 7, 9};
- 
-    while (true) {
-	for (int i = 0; i < sizeof(device_id)/sizeof(device_id[0]); i ++) {
-		int blink_count =  device_id[i];
-		for (int b = 0; b < blink_count; b++) {
-			bsp_board_led_invert(i);
-		 	nrf_delay_ms(500);
-			bsp_board_led_invert(i);
-			nrf_delay_ms(500);
-		}
-		nrf_delay_ms(2500);
-	}
-    }
+    nrf_gpio_cfg_output(FIRST_LED_PIN);
+    nrf_gpio_cfg_output(SECOND_RED_LED_PIN);
+    nrf_gpio_cfg_output(SECOND_GREEN_LED_PIN);
+    nrf_gpio_cfg_output(SECOND_BLUE_LED_PIN);
+    nrf_gpio_cfg_input(BUTTON_PIN, NRF_GPIO_PIN_PULLUP);
 }
 
-/**
- *@}
- **/
+void leds_off(void)
+{
+    nrf_gpio_pin_write(FIRST_LED_PIN, 1);
+    nrf_gpio_pin_write(SECOND_RED_LED_PIN, 1);
+    nrf_gpio_pin_write(SECOND_GREEN_LED_PIN, 1);
+    nrf_gpio_pin_write(SECOND_BLUE_LED_PIN, 1);
+}
+
+int main(void)
+{
+    gpio_init();
+    leds_off(); 
+
+    int current_led = 0; 
+    int blink_index = 0; 
+    bool led_on = false; 
+
+    while (true)
+    {
+    
+        if (!nrf_gpio_pin_read(BUTTON_PIN)) 
+        {
+        
+            if (blink_index < device_id[current_led])
+            {
+                nrf_gpio_pin_write(led_sequence[current_led], 0); 
+                nrf_delay_ms(LED_DELAY_MS);
+                leds_off(); 
+                nrf_delay_ms(LED_DELAY_MS);
+
+                blink_index++; 
+            }
+            else {
+
+                blink_index = 0; 
+                current_led = (current_led + 1) % (sizeof(device_id) / sizeof(device_id[0])); 
+            }
+
+            led_on = true; 
+        }
+        else 
+        {
+            if (led_on) 
+            {
+                nrf_gpio_pin_write(led_sequence[current_led], 0); 
+            }
+            else 
+            {
+                leds_off();
+            }
+        }
+
+        nrf_delay_ms(LED_DELAY_MS); 
+    }
+}
